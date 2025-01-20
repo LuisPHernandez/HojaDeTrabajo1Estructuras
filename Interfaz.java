17c5fe268b5c11110c034326bbdfa7abfbde11a9
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-01-20 17:19:57 -0600

    Creación de la interfaz

---
+++ Interfaz.java
@@ -0,0 +1,29 @@
+// -----------------------------------
+// Pedro Caso
+// Pablo Toledo
+// Luis Hernandez 
+// Algoritmos y estructuras básicas
+// HDT1
+// -----------------------------------
+
+package HojaDeTrabajo1Estructuras;
+
+interface Interfaz {
+    // metodo para encender la licuadora
+    void encender();
+
+    // método para llenar la licuadora
+    void llenar();
+
+    // método para aumentar la velocidad y disminuir la velocidad
+    void aumentarVelocidad();
+
+    // método para verificar el estado de la velocidad actual
+    int consultarVelocidad();
+
+    // método para consultar si la licuadora se encuentra llena
+    boolean consultarLlenado();
+
+    // método para vaciar la licuadora
+    void vaciar();
+}
