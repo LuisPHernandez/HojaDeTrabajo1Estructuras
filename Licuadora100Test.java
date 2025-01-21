8cd47cfba2420497ca1bdddf86719d1ee5af45ee
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-01-20 22:45:45 -0600

    Pruebas Test para JUnit y Cambios en la documentación

--- Licuadora100Test.java
+++ Licuadora100Test.java
@@ -1,10 +1,74 @@
 package HojaDeTrabajo1Estructuras;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class Licuadora100Test {
 
+    private Licuadora100 licuadora;
 
+    // se inicializa y crea una licuadora antes de cada prueba (método)
+    @BeforeEach
+    public void TestLicuadora100() {
+        licuadora = new Licuadora100();
+    }
+
+    // Este test será para probar su encendido
+    @Test
+    public void testParaEncendido() {
+        licuadora.llenar(); // primero se llena la licuadora
+        licuadora.encender(); // luego se enciende
+        assertTrue(licuadora.isEncendido());
+        assertEquals(1, licuadora.getVelocidad(), "La velocidad luego de encenderse es 1 (vel. mínima)");
+    }
+
+    // El segundo test es para apagar la licuadora
+    @Test
+    public void testParaApagado() {
+        licuadora.llenar(); // se llena para encenderse
+        licuadora.encender(); // se enciende
+        licuadora.encender(); // se vuelve a presionar la misma opcion para apagarse
+        assertFalse(licuadora.isEncendido());
+        assertEquals(0, licuadora.getVelocidad(), "la velocidad tiene que ser 0 ya que esta apagada");
+    }
+
+    // El tercer test es para aumentar la velocidad de 1 en 1
+    @Test
+    public void testParaAumentarVelocidad() {
+        licuadora.llenar(); // se llena para encenderse 
+        licuadora.encender(); // se enciende (vel 1)
+        licuadora.aumentarVelocidad(); // ahora se aumenta la velocidad (vel 2)
+        assertEquals(2, licuadora.getVelocidad(), "la velocidad tiene que ser 2 despues de aumentar 1 vez la velocidad");
+    }
+
+    // El cuarto test es para disminuir la velocidad una vez llegue a 10
+    @Test
+    public void testParaReducirVelMax() {
+        licuadora.llenar(); // se llena para encenderse
+        licuadora.encender(); // se enciende
+        // se aumenta la velocidad hasta 10 para que se disminuya a 1 nuevamente
+        for (int i = 0; i < 10; i++) { 
+            licuadora.aumentarVelocidad();
+        }
+        assertEquals(1, licuadora.getVelocidad());
+    }
+
+    // El quinto test es para verificar si la licuadora esta llena o vacía
+    @Test
+    public void testVerificarLlenado() {
+        assertFalse(licuadora.consultarLlenado(), "Tiene que estar vacía para llenarse");
+        licuadora.llenar(); // se llena la licuadora
+        assertTrue(licuadora.consultarLlenado());
+    }
+
+    // 
+    @Test
+    public void testParaVaciarla() {
+        licuadora.llenar(); // se llena la licuadora
+        assertTrue(licuadora.consultarLlenado(), "tiene que estar llena para vaciarse");
+        licuadora.vaciar(); // se vacía la licuadora
+        assertFalse(licuadora.consultarLlenado());
+    }
 }
--- Main.java
+++ Main.java
@@ -6,51 +6,67 @@ public class Main {
     public static void main(String []args){
         Licuadora100 licuadora = new Licuadora100();
         Scanner scan = new Scanner(System.in);
-        boolean licuar = true;
-        while(licuar != false){
-            System.out.println("Opciones de licuadora: ");
-            System.out.println("1. Encender Licuadora");
-            System.out.println("2. Llenar licuadora");
-            System.out.println("3. Aumentar la velocidad");
-            System.out.println("4. Consultar velocidad");
+        boolean continuar = true;
+        while(continuar != false){
+            System.out.println("opciones ofrecidas por la licuadora");
+            System.out.println("1. Encender Licuadora (tiene que llenarse primero)");
+            System.out.println("2. Llenar la licuadora");
+            System.out.println("3. Aumentar/disminuir la velocidad (de 1 a 10)");
+            System.out.println("4. Consultar velocidad actual");
             System.out.println("5. Consultar llenado");
-            System.out.println("6. Vaciar");
+            System.out.println("6. Vaciar licuadora");
             System.out.println("7. Salir");
 
-            int op = scan.nextInt();
-            switch(op){
+            int opciones = scan.nextInt();
+            switch(opciones){
+
+                //Encender la licuadora
                 case 1:
                     licuadora.encender();
                     if (licuadora.isEncendido()) {
-                        System.out.println("Licuadora encendida");
-                    } else {
-                        System.out.println("Licuadora apagada");
+                        System.out.println("La licuadora se encendió");
+                    } 
+                    
+                    else {
+                        System.out.println("La licuadora se apagó");
                     }
                     break;
+
+                // Llenar la licuadora 
                 case 2:
                     licuadora.llenar();
-                    System.out.println("Licuadora llena.");
+                    System.out.println("La licuadora se llenó con éxito");
                     break;
+
+                // Aumentar/Disminuir velocidad
                 case 3:
                     licuadora.aumentarVelocidad();
-                    System.out.println("Velocidad actual: " + licuadora.consultarVelocidad());
+                    System.out.println("La velocidad aumentó/disminuyó a: " + licuadora.consultarVelocidad());
                     break;
+
+                // Consultar la velocidad actual
                 case 4:
-                    System.out.println("Velocidad actual: " + licuadora.consultarVelocidad());
+                    System.out.println("La velocidad actual es: " + licuadora.consultarVelocidad());
                     break;
+
+                // Verificar si esta llena o vacía
                 case 5:
-                    System.out.println("Estado de llenado: " + (licuadora.consultarLlenado() ? "Lleno" : "Vacío"));
+                    System.out.println("Estado: " + (licuadora.consultarLlenado() ? "Llena" : "Vacía"));
                     break;
+                
+                // Vaciar licuadora 
                 case 6:
                     licuadora.vaciar();
-                    System.out.println("Licuadora vaciada.");
+                    System.out.println("La licuadora se vació.");
                     break;
+
+                // Salir del programa 
                 case 7:
-                    licuar = false;
-                    System.out.println("Saliendo del programa...");
+                    continuar = false;
+                    System.out.println("Saliendo...");
                     break;
                 default:
-                    System.out.println("Opción no válida. Intente de nuevo.");
+                    System.out.println("La opción elegida no es válida");
             }
         }
         scan.close();
