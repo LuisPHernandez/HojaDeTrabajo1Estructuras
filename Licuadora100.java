75d29dd69ef2380adb8d864bcd52d526461665c4
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-01-20 19:06:38 -0600

    Implementacion de los metodos en la clase Licuadora

--- Licuadora100.java
+++ Licuadora100.java
@@ -6,9 +6,36 @@ public class Licuadora100 implements Interfaz{
     private int velocidad;
     
 
-    public Licuadora(boolean encendido, boolean lleno, int velocidad){
-        this.encendido = encendido;
-        this.lleno = lleno;
-        this.velocidad = velocidad;
+    public Licuadora100() {
+        this.encendido = false;
+        this.lleno = false;
+        this.velocidad = 0;
     }
+
+    // requisito previo, tiene que estar apagada y llena
+    @Override
+    public void encender() {
+        if (encendido == false && lleno == true) {
+            encendido = true;
+            velocidad = 1; // se enciende con la velocidad 1, ya que no es posible saltarse velocidades
+        }
+    }
+
+    @Override
+    public void llenar() {
+        lleno = true;
+    }
+
+    // requisitos previo, solo se puede aumentar la velocidad si esta encendida la licuadora y cuando llegue a 10 regrese a la velocidad 1
+    @Override
+    public void aumentarVelocidad() {
+        if(encendido == true) {
+            velocidad += 1;
+            if (velocidad > 10) {
+                velocidad = 1;
+            }
+        }
+    }
+
+
 }
